f0578707a24923f8449ac7e925c8a7253c28c0c1
PAPERPANKS <pnkjgautam05@gmail.com>
2017-10-03 21:02:40 +0530

    adding main files

---
+++ header.php
@@ -0,0 +1,207 @@
+<div class="header">
+<nav class="navbar navbar-primary navbar-transparent navbar-fixed-top navbar-color-on-scroll">
+<div class="container">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand brand-name">
+            <!-- <img class="img" src="https://www.apjanmabhoomi.org/wp-content/uploads/2017/01/ap-janmabhoomi.png" alt="APJanmabhoomi"> -->
+        </a>
+    </div>
+    
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-center">
+            <li>
+                <a href="index.php">                 
+                    HOME   
+
+                </a>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">                 
+                    ABOUT US
+                <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="">                                          
+                            CO-ORDINATORS
+                    
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">                                          
+                            INTERNS
+                    
+                        </a>
+                    </li>                                                                                         
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">                 
+                    PROJECTS
+                <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="">                                        
+                           DIGITAL CLASSROOM                  
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">                                       
+                            ANGANWADI CENTERS               
+                        </a>
+                    </li>                                      
+                    <li>
+                        <a href="">                                        
+                            CREMATORIUMS               
+                        </a>
+                    </li>                                                      
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    INTERNSHIP
+                </a>               
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">                   
+                    ANALYTICS
+                </a>             
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    MEDIA CORNER
+                <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="">                                          
+                            GALLERY                
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">                                          
+                            SUCCESS STORIES                   
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">                                          
+                            BLOG
+                    
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">                                          
+                            APJanmabhoomi IN NEWS
+                    
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">                                          
+                            NEWSLETTERS
+                    
+                        </a>
+                    </li>                                                                                      
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    CONTACT US            
+                </a>              
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    REGISTER               
+                </a>            
+            </li>	
+        </ul>
+    </div>
+</div>
+</nav>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <div class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2" class="active"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item">
+                <div class="page-header header-filter" style="background-image: url('https://www.apjanmabhoomi.org/wp-content/uploads/2016/12/video-bg.jpg');">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 text-left">
+                                <h1 class="title">Title 1</h1>
+                                <h4></h4>
+                                <br>
+
+                                <div class="buttons">
+                                    
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="item">
+                <div class="page-header header-filter" style="background-image: url('https://www.apjanmabhoomi.org/wp-content/uploads/2017/01/digital-class-banner.jpg');">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-md-offset-2 text-center">
+                                <h1 class="title">Title 2</h1>
+                                <h4></h4>
+                                <br>                              
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="item active">
+                <div class="page-header header-filter" style="background-image: url('https://www.apjanmabhoomi.org/wp-content/uploads/2016/12/digital-class2.jpg');">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7 col-md-offset-5 text-right">
+                                <h1 class="title">Title 3</h1>
+                                <h4></h4>
+                                <br>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+            <i class="material-icons">keyboard_arrow_left</i>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+            <i class="material-icons">keyboard_arrow_right</i>
+        </a>
+    </div>
+</div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>IPU-Home</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="shortcut icon" href="#.ico">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/material-kit.min.css" rel="stylesheet">
+
+	<style>
+		/* .cards .container-fluid .row .col-sm-6{
+			padding-left:0;
+			padding-right:0;
+		} */
+	</style>
+</head>
+<body class="index-page">
+    <?php require_once ('header.php'); ?>
+
+    
+
+	<div class="main">
+
+		<div class="section text-center section-landing">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h2 class="title">About AP Janmabhoomi</h2>
+					<h5 class="description">
+						The Government of Andhra Pradesh, alongside <strong>The Special Representative for North 
+						America</strong> endeavors to bridge NRIs to the native land. <strong>AP JANMABHOOMI</strong>, 
+						a social initiative to give back to the motherland, comprises of three schemes- <strong>Digital Classrooms, 
+						Anganwadi Centers and Crematoriums</strong>. With no Government who has ever 
+						taken such a dynamism, the Andhra Pradesh Government provides this platform by providing 70% of the total cost. 
+						The rest 30% is funded by NRIs who want to help the motherland.<br>
+						his platform intents on working for the advancement of the Sunrise state. Digital Classrooms 
+						establish a stage to match up global levels of education. Anganwadi Centers promote preschool 
+						education, healthcare and hygiene for children and pregnant women. Crematoriums ensures the last rites are paid 
+						right. The Andhra Pradesh government and the Special Representative for North America not only facilitates the progress of the 
+						projects but also assures that each donation is executed. 
+						So come along, be a part of many more firsts, support us in our dream to make our Janmabhoomi a superior place to live
+					</h5>
+				</div>
+			</div>
+		</div><!--section-landing-->
+
+		<div id="cards" class="cd-section">
+			<div class="section-gray">
+				<div class="cards" >
+					<div class="container-fluid">
+						<div class="title">
+							<h2>Adding more content ...</h2>
+							<!--h3>Quick Links</h3-->
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div><!--cards-->
+
+		<?php require_once ('./#.html'); ?> <!--footer page-->
+
+	</div><!--main-raised-->
+
+	<!--   Core JS Files   -->
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="assets/js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		// $(document).ready(function(){		
+		// 	$(".dropdown").hover(function () {
+		// 		$(this).toggleClass("open");
+		// 	});				
+		// });
+    </script>
+</body>
+</html>
