4500045e187ec1bbcc3518328c6c812207033c18
PAPERPANKS <pnkjgautam05@gmail.com>
2017-10-05 21:00:48 +0530

    adding a gallery page

---
+++ gallery.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>APJanmabhoomi</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="shortcut icon" href="#.ico">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/material-kit.min.css" rel="stylesheet">
+
+</head>
+<body style="padding-top:20vh;" class="index-page">
+    <?php require_once ('header.php'); ?>
+
+<div class="main main-raised">
+    
+    <div class="section section-sections">
+        <div class="container">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-description">
+                    <h2 class="title">Gallery</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>             
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/gallery/international-yoga-day/IMG-20170621-WA0080.jpg" alt="">
+                </div>
+            </div>           
+        </div>
+    </div>
+    
+</div>
+<!--   Core JS Files   -->
+<script src="./js/jquery.min.js" type="text/javascript"></script>
+	<script src="./js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="./js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="./js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		// $(document).ready(function(){		
+		// 	$(".dropdown").hover(function () {
+		// 		$(this).toggleClass("open");
+		// 	});				
+		// });
+    </script>
+</body>
+</html>
