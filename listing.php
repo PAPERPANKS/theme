5fc3d800b9c2cf6f756b7f4eea8ed84850bdc56c
PAPERPANKS <pnkjgautam05@gmail.com>
2017-10-05 20:55:34 +0530

    adding basic table

---
+++ listing.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>APJanmabhoomi</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="shortcut icon" href="#.ico">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/material-kit.min.css" rel="stylesheet">
+
+</head>
+<body style="padding-top:20vh;"  class="index-page">
+    <?php require_once ('header.php'); ?>
+    
+    <div class="main">
+        <div class="section">
+            <div class="container tim-container">
+
+                <div id="contentAreas" class="cd-section">
+
+                    <h2>School Listing</h2>
+
+                    <div id="tables">
+                        
+                        <div class="row">                           
+                            <div class="col-md-10 col-md-offset-1">
+                                <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">School Code</th>
+                                            <th>School Name</th>
+                                            <th>District</th>
+                                            <th>Assembly</th>
+                                            <th>Mandal</th>
+                                            <th class="text-right">Village</th>
+                                            <th class="text-right">Donate</th>                                           
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="text-center">1</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td class="text-right"></td>
+                                            <td class="text-right"></td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" class="btn btn-success" data-original-title="" title="">
+                                                    <i class="material-icons">send</i>
+                                                </button>                                               
+                                            </td>
+                                        </tr>                                  
+                                        <tr>
+                                            <td class="text-center">2</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td class="text-right"></td>
+                                            <td class="text-right"></td>
+                                            <td class="td-actions text-right">
+                                                <button type="button" rel="tooltip" class="btn btn-success" data-original-title="" title="">
+                                                    <i class="material-icons">send</i>
+                                                </button>
+                                                
+                                            </td>
+                                        </tr>                   
+                                    </tbody>
+                                </table>
+                                </div>                               
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    <div>
+    
+    <script src="./js/jquery.min.js" type="text/javascript"></script>
+	<script src="./js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="./js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="./js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		// $(document).ready(function(){		
+		// 	$(".dropdown").hover(function () {
+		// 		$(this).toggleClass("open");
+		// 	});				
+		// });
+    </script>
+</body>
+
+</html>
