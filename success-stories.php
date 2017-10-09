90b8ec8e656b8aa375284a8e24a9cf71578cc096
PAPERPANKS <pnkjgautam05@gmail.com>
2017-10-09 17:01:03 +0530

    added success story page

---
+++ success-stories.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>APJanmabhoomi</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="shortcut icon" href="#.ico">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/material-kit.min.css" rel="stylesheet">
+
+	<style>
+        .container-fluid{
+            padding:20px !important;
+        }
+		.card img{
+			width:50% !important;
+		}
+	</style>
+</head>
+<body style="padding-top:20vh;" class="index-page">
+    <?php require_once ('header.php'); ?>
+   
+	<div class="main main-raised">
+
+        <div class="container-fluid">          
+            <div class="row">
+            </div>
+			<div class="row">
+                <div class="col-md-4 ">
+                    <div class="card">
+                        <div class="card-content">
+                            <h6 class="category">
+                                <i class="material-icons">mail</i> Mail
+                            </h6>
+                            
+                            <p class="text-info">NRIs have always been contributing either directly or indirectly for various social welfare &amp charitable 
+                            activities in their native village/town. This section is to highlight such projects/contributions which 
+                            were previously completed. Please let us know if you or someone you know would like to showcase and set 
+                            example of such previous projects.<br/>
+                            This helps to build awareness and encourage others. Please send us an email with the information to
+                            <a href="mailto:info@apjanmabhoomi.org">info@apjanmabhoomi.org</a>
+                            </p>
+                                             
+                        </div>
+                    </div>
+                </div>
+				<div class="col-md-8 ">
+					<div class="card card-plain">
+						<div class="image">
+							<a href="#pablo">
+								<img class="img img-raised" src="https://www.apjanmabhoomi.org/wp-content/uploads/2016/12/9739c56a-ee1b-474d-bfc9-6cf22a5ffca7-300x196.png">
+							</a>
+                        </div>
+
+						<h3 class="title">
+							Project Details
+						</h3>
+
+						<h5 class="card-description">
+                            <p>AP Janmabhoomi Success StoriesSpandana strives to mainstream children with disabilities (CWDs). 
+                            Many of these children are orphans and most are unable to go through the basic routine of a day without 
+                            considerable help. In addition to speech and physical therapy, it provides food, shelter and clothing to 
+                            more than 40 physically challanged children.</p>
+						</h5>
+
+						<h3 class="title">
+							Key Contribution
+						</h3>
+
+						<h5 class="card-description">
+                            <p>Most of the NRIs living especially in western countries could easily compare the kind of facilities and life a mentally/physically challenged person get there when compared to India. For a country with over 1.2 billion population, you hardly would see options/places that offer any kind of care to such people.
+                            <br/>
+                            Raji Muppala, an NRI living in Kansas (USA), has been helping to sustain and develop this 
+                            home  since 2009. Instead of just contributing some money like most, she has been 
+                            coordanating the fund raising, planning and monitoring the execution of various activities 
+                            working through Asha for Education. She has been instrumental in realizing the dream of own 
+                            building, getting faculty and equipment for rehabilitation. Her dedicated contributions were 
+                            recognized by Sprint, the company she works for, through the Community Leadership Award 
+                            presented by Dan Hesse (CEO of Sprint in 2012).</p>
+						</h5>
+
+						<h3 class="title">
+							Project Details
+						</h3>
+
+						<h5 class="card-description">
+                            <p>You can donate monthly towards speech and physiotherapist through recurring donations. 
+                            You could make your donations through Asha for Education, a fedarally tax-exempt 
+                            organization as per IRS 501(c). So, all contributions are eligible for tax deduction.</p>
+						</h5>
+
+					</div>
+					
+				</div>
+
+			</div>
+
+		</div>
+		<?php //require_once ('./#.html'); ?> <!--footer page-->
+
+	</div><!--main-raised-->
+
+	<!--   Core JS Files   -->
+	<script src="./js/jquery.min.js" type="text/javascript"></script>
+	<script src="./js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="./js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="./js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		// $(document).ready(function(){		
+		// 	$(".dropdown").hover(function () {
+		// 		$(this).toggleClass("open");
+		// 	});				
+		// });
+    </script>
+</body>
+</html>
