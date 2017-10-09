41ee054678a413b7294dcaf723dbe10103160b16
PAPERPANKS <pnkjgautam05@gmail.com>
2017-10-09 15:08:04 +0530

    addede inters page

---
+++ interns.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>APJanmabhoomi</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="shortcut icon" href="#.ico">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/material-kit.min.css" rel="stylesheet">
+
+	<style>
+		.card-avatar img{
+			height:147px! important;			
+		} 
+	</style>
+</head>
+<body style="padding-top:20vh;" class="index-page">
+    <?php require_once ('header.php'); ?>
+   
+	<div class="main main-raised">
+
+        <div id="cards" class="cd-section">
+            <div class="cards">
+                <div class="container">
+                    <div class="title">
+                        <h1>Core Team</h1>
+                    </div>
+                    <br/>
+                    <div class="row">
+                        <!--add col here don't add row just only column--> 
+                        <div class="col-md-4">
+                            <div class="card card-profile">
+                                <div class="card-avatar">
+                                    <a href="#">
+                                        <img class="img" src="https://www.apjanmabhoomi.org/wp-content/uploads/2017/06/K.Arpitha.jpg">
+                                    </a>                              
+                                </div>
+
+                                <div class="card-content">
+                                    <h4 class="card-title">Name</h4>
+                                    <h6 class="category text-gray">Work</h6>
+                                    <h5 class="category text-gray">College, Location</h5>
+
+                                    <div class="footer">
+                                        <a href="#pablo" class="btn btn-just-icon btn-twitter btn-round"><i class="fa fa-linkedin"></i></a>
+                                        <a href="#pablo" class="btn btn-just-icon btn-google btn-round"><i class="fa fa-github"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class="title">
+                        <h1>Policy Research Interns</h1>
+                    </div>
+                    <br/>
+                    <div class="row">
+                        <!--add col here don't add row just only column--> 
+                        <div class="col-md-4">
+                            <div class="card card-profile">
+                                <div class="card-avatar">
+                                    <a href="#">
+                                        <img class="img" src="https://www.apjanmabhoomi.org/wp-content/uploads/2017/09/Mohit_Choube_IIITDM-Jabalpur_Ghaziabad_UP_Policy-Research-Intern.jpg">
+                                    </a>                              
+                                </div>
+
+                                <div class="card-content">
+                                    <h4 class="card-title">Name</h4>
+                                    <h6 class="category text-gray">Work</h6>
+                                    <h5 class="category text-gray">College, Location</h5>
+
+                                    <div class="footer">
+                                        <a href="#pablo" class="btn btn-just-icon btn-twitter btn-round"><i class="fa fa-linkedin"></i></a>
+                                        <a href="#pablo" class="btn btn-just-icon btn-google btn-round"><i class="fa fa-github"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class="title">
+                        <h1>Main Team</h1>
+                    </div>
+                    <br/>
+                    <div class="row">
+                        <!--add col here don't add row just only column--> 
+                        <div class="col-md-4">
+                            <div class="card card-profile">
+                                <div class="card-avatar">
+                                    <a href="#">
+                                        <img class="img" src="https://www.apjanmabhoomi.org/wp-content/uploads/2017/07/Sumit-Dargan-1.jpg">
+                                    </a>                              
+                                </div>
+
+                                <div class="card-content">
+                                    <h4 class="card-title">Name</h4>
+                                    <h6 class="category text-gray">Work</h6>
+                                    <h5 class="category text-gray">College, Location</h5>
+
+                                    <div class="footer">
+                                        <a href="#pablo" class="btn btn-just-icon btn-twitter btn-round"><i class="fa fa-linkedin"></i></a>
+                                        <a href="#pablo" class="btn btn-just-icon btn-google btn-round"><i class="fa fa-github"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class="title">
+                        <h1>NRI Interns</h1>
+                    </div>
+                    <br/>
+                    <div class="row">
+                        <!--add col here don't add row just only column--> 
+                        <div class="col-md-4">
+                            <div class="card card-profile">
+                                <div class="card-avatar">
+                                    <a href="#">
+                                        <img class="img" src="https://www.apjanmabhoomi.org/wp-content/uploads/2017/08/Minisha-Trivedi.jpg">
+                                    </a>                              
+                                </div>
+
+                                <div class="card-content">
+                                    <h4 class="card-title">Name</h4>
+                                    <h6 class="category text-gray">Work</h6>
+                                    <h5 class="category text-gray">College, Location</h5>
+
+                                    <div class="footer">
+                                        <a href="#" class="btn btn-just-icon btn-twitter btn-round"><i class="fa fa-linkedin"></i></a>
+                                        <a href="#" class="btn btn-just-icon btn-google btn-round"><i class="fa fa-github"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+		<?php //require_once ('./#.html'); ?> <!--footer page-->
+
+	</div><!--main-raised-->
+
+	<!--   Core JS Files   -->
+	<script src="./js/jquery.min.js" type="text/javascript"></script>
+	<script src="./js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="./js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="./js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		// $(document).ready(function(){		
+		// 	$(".dropdown").hover(function () {
+		// 		$(this).toggleClass("open");
+		// 	});				
+		// });
+    </script>
+</body>
+</html>
