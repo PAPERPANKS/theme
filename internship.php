a501d1d30a928c8a3737b52d8f651c53a34226ab
PAPERPANKS <pnkjgautam05@gmail.com>
2017-10-09 19:04:47 +0530

    added internship page

---
+++ internship.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>APJanmabhoomi</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="shortcut icon" href="#.ico">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/material-kit.min.css" rel="stylesheet">
+    <style>
+        .description{
+            margin:10px;
+        }
+    </style>
+</head>
+<body style="padding-top:20vh;" class="index-page">
+
+    <?php require_once ('header.php'); ?>
+
+<div class="main">
+
+    <div class="section section-sections">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <img src="https://www.apjanmabhoomi.org/wp-content/uploads/2017/01/WhatsApp-Image-2017-08-01-at-9.24.58-PM.jpeg">            
+                    <br/>
+                    <h5 class="description"><span style="font-size:35px; color:green">" </span>Come work with the most operative and progressive government of Andhra Pradesh in their innovative initiative 
+                    of ‘AP Janambhoomi’. We are looking for bright young minds who are enthusiastic to work with us. If you want to 
+                    make use of your skills and learn something new at the same time, then join AP Janambhoomi 
+                    and get to work closely with the government itself. We assure you, it will be 
+                    a great learning opportunity!<span style="font-size:35px; color:green"> "</span>
+                    </h5>
+                </div>
+            </div>
+            <hr/>
+        
+            <div class="row">
+                
+				<div class="col-md-4 col-md-offset-2">
+					<div class="card card-pricing card-raised">
+						<div class="card-content">							
+							<h3 class="card-title">Human Resources (HR)</h3>
+                                <ol>
+                                    <li>Handle end to end recruitment process</li><hr/>
+                                    <li>Map and monitor the performance of the team, in charge 
+                                    (The team will consist of not more than 10 members and they will be from 
+                                    different departments of the project)</li><hr/>
+                                    <li>Take daily updates from the team members and ensure a proper 
+                                    coordination among them</li>
+                                </ol>
+							<a href="#" class="btn btn-rose btn-success">Apply</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="card card-pricing card-raised">
+						<div class="card-content">							
+							<h3 class="card-title">Social Media Marketing</h3>							
+                                <ol>
+                                    <li>Come up with innovative campaigns to take the initiative of APJanmabhoomi forward</li><hr/>
+                                    <li>Work with the teams virtually and get the campaigns executed</li><hr/>
+                                    <li>Analyze the outreach and come up with strategies to improve them</li><hr/>
+                                    <li>Handle social media marketing</li>
+                                </ol>
+							<a href="#" class="btn btn-rose btn-success">Apply</a>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div><!--container-->
+    </div><!--secton-->
+    
+</div>
+<!--   Core JS Files   -->
+<script src="./js/jquery.min.js" type="text/javascript"></script>
+	<script src="./js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="./js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="./js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		// $(document).ready(function(){		
+		// 	$(".dropdown").hover(function () {
+		// 		$(this).toggleClass("open");
+		// 	});				
+		// });
+    </script>
+</body>
+</html>
